4b0c7a46fffa3040be72aa2503f67197d6fb0310
mohammedZ007 <mohdzakkis@gmail.com>
2026-09-11 00:30:52 +0530

    Add Terraform EC2 infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,72 @@
+# Get the latest Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+
+# Security Group
+resource "aws_security_group" "crecita_sg" {
+  name        = "crecita-security-group"
+  description = "Allow SSH and React application traffic"
+
+  # SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # React application
+  ingress {
+    description = "React application"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "crecita-security-group"
+  }
+}
+
+
+# EC2 Instance
+resource "aws_instance" "crecita_server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.crecita_sg.id]
+
+  tags = {
+    Name = "crecita-server"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+  default     = "linux-for-devops-key"
+}   
