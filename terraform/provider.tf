fe4ebf1ff6f1c6983f8ecab6aaf915c2cdce3b98
mohammedZ007 <mohdzakkis@gmail.com>
2026-09-11 01:15:57 +0530

    Configure Terraform remote state with S3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "s3" {
+    bucket       = "crecita-terraform-state-459969247850"
+    key          = "terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
+}
+
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
